d13927be1c9cf4495d81479cd95bb8e735620218
kzlsakal <dorukgurleyen@gmail.com>
2020-08-26 11:18:05 -0700

    Add insertListing db method

--- database/helpers.js
+++ database/helpers.js
@@ -19,6 +19,38 @@ const queryTableDataFromID = (id, callback) => {
     });
 };
 
+const insertListing = (listing, callback) => {
+  let conn;
+  mariadb.createConnection(dbConnectionOptions)
+    .then((connection) => {
+      conn = connection;
+      const queryString = 'INSERT INTO listings (\
+        ownerName, \
+        rating, \
+        numRatings, \
+        pricePerNight, \
+        discountAmount, \
+      )';
+      const queryArgs = [
+        listing.ownerName,
+        listing.rating,
+        listing.numRatings,
+        listing.pricePerNight,
+        listing.discountAmount
+      ];
+      return conn.query(queryString, queryArgs);
+    })
+    .then((results) => {
+      conn.close();
+      callback(null, results);
+    })
+    .catch((err) => {
+      conn.close();
+      callback(err, null);
+    });
+};
+
 module.exports = {
   queryTableDataFromID,
+  insertListing,
 };
