79d0b9630bb7d76aa3eb00af6863bcab975484ac
kzlsakal <dorukgurleyen@gmail.com>
2020-08-29 20:49:58 -0700

    Implement db methods and api routing for postgres

---
+++ server/database/index.js
@@ -0,0 +1,107 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'kizilsakal',
+  password: 'nopassword',
+  database: 'booking_db',
+  ssl: { rejectUnauthorized: false }
+});
+
+const getListing = (id, callback) => {
+  const queryString = 'SELECT * FROM listings WHERE id=$1';
+  const queryArgs = [Number(id)];
+  pool.connect()
+    .then(client => {
+      return client.query(queryString, queryArgs)
+        .then(result => {
+          client.release();
+          callback(null, result.rows);
+        })
+        .catch(error => {
+          client.release();
+          callback(error, null);
+        });
+    });
+};
+
+const insertListing = (
+  {ownerName, rating, numRatings, pricePerNight, discountAmount }, callback
+) => {
+  const queryString = 'INSERT INTO listings ('
+  + '"ownerName", "rating", "numRatings", "pricePerNight", "discountAmount")'
+  + ' VALUES ($1, $2, $3, $4, $5) RETURNING *';
+  const queryArgs = [ownerName, rating, numRatings, pricePerNight, discountAmount];
+  pool.connect()
+    .then (client => {
+      client.query(queryString, queryArgs)
+        .then(results => {
+          client.release();
+          callback(null, results.rows);
+        })
+        .catch(error => {
+          console.log(error);
+          client.release();
+          callback(error, null);
+        });
+    });
+};
+
+const modifyListing = (listing, callback) => {
+  const options = {
+    'ownerName': 1,
+    'rating': 1,
+    'numRatings': 1,
+    'pricePerNight': 1,
+    'discountAmount': 1
+  };
+  const queryArgs = [Number(listing.id)];
+  const columns = [];
+  const params = [];
+  let idx = 2;
+  for (let key in listing) {
+    if (options[key]) {
+      columns.push(`"${key}"`);
+      params.push('$' + idx);
+      idx++;
+      queryArgs.push(listing[key]);
+    }
+  }
+  const queryString = `UPDATE listings SET (id, ${columns.toString()}) `
+  + `= ($1, ${params.toString()}) WHERE id = $1 RETURNING *`;
+  pool.connect()
+    .then(client => {
+      return client.query(queryString, queryArgs)
+        .then(result => {
+          client.release();
+          callback(null, result.rows);
+        })
+        .catch(error => {
+          client.release();
+          callback(error, null);
+        });
+    });
+};
+
+const deleteListing = (id, callback) => {
+  const queryString = 'DELETE FROM listings WHERE id=$1 RETURNING *';
+  const queryArgs = [Number(id)];
+  pool.connect()
+    .then(client => {
+      return client.query(queryString, queryArgs)
+        .then(result => {
+          client.release();
+          callback(null, result.rows);
+        })
+        .catch(error => {
+          console.log(error);
+          client.release();
+          callback(error, null);
+        });
+    });
+};
+
+module.exports = {
+  getListing,
+  insertListing,
+  modifyListing,
+  deleteListing,
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('../database/helpers');
+const db = require('./database');
 const cors = require('cors');
 
 const app = express();
@@ -13,27 +13,27 @@ app.use('/(:id)', express.static(path.join(__dirname, '../public')));
 
 app.get('/api/booking/:id', (req, res) => {
   const { id } = req.params;
-  db.queryTableDataFromID(id, (listingErr, listing) => {
-    if (listingErr) {
-      console.error(`Error querying listing in database: ${listingErr}`);
-      res.status(500).send(listingErr);
-    } else {
-      res.status(200).send({
-        listing,
-      });
+  db.getListing(id, (error, listing) => {
+    if (error) {
+      res.status(500).end('There was an error. Please try again later.');
+      return;
     }
+    if (!listing.length) {
+      res.status(404).end('Listing was not found.');
+      return;
+    }
+    res.status(200).send({ listing }).end();
   });
 });
 
-app.post('/api/booking/:id', (req, res) => {
-  const { id } = req.params;
-  const listing = req.body;
-  db.insertListing(listing, (error, result) => {
+app.post('/api/booking', (req, res) => {
+  const newListing = req.body;
+  db.insertListing(newListing, (error, listing) => {
     if (error) {
-      res.status(500).end('There was an error. Please try again later.');
+      res.status(500).send('There was an error. Please try again later.');
+      return;
     } else {
-      listing.id = result.insertId;
-      res.status(200).send({listing: [listing]}).end();
+      res.status(201).send({ listing }).end();
     }
   });
 });
@@ -41,45 +41,32 @@ app.post('/api/booking/:id', (req, res) => {
 app.put('/api/booking/:id', (req, res) => {
   const { id } = req.params;
   const modifiedListing = req.body;
-  db.queryTableDataFromID(id, (error, results) => {
-    const listing = results[0];
-    if (!listing) {
-      res.status(404).end('Listing was not found.');
-    }
+  modifiedListing.id = id;
+  db.modifyListing(modifiedListing, (error, listing) => {
     if (error) {
       res.status(500).end('There was an error. Please try again later.');
-    } else {
-      delete modifiedListing.id;
-      Object.assign(listing, modifiedListing);
-      db.modifyListing(listing, (error, result) => {
-        if (error) {
-          res.status(500).end('There was an error. Please try again later.');
-        } else {
-          res.status(200).send({listing: [listing]}).end();
-        }
-      });
+      return;
     }
+    if (!listing.length) {
+      res.status(404).end('Listing was not found.');
+      return;
+    }
+    res.status(200).send({ listing }).end();
   });
 });
 
 app.delete('/api/booking/:id', (req, res) => {
   const { id } = req.params;
-  db.queryTableDataFromID(id, (error, results) => {
-    const listing = results[0];
-    if (!listing) {
-      res.status(404).end('Listing was not found.');
-    }
+  db.deleteListing(id, (error, listing) => {
     if (error) {
       res.status(500).end('There was an error. Please try again later.');
-    } else {
-      db.deleteListing(id, (error, result) => {
-        if (error) {
-          res.status(500).end('There was an error. Please try again later.');
-        } else {
-          res.status(200).send({listing: [listing]}).end();
-        }
-      });
+      return;
     }
+    if (!listing.length) {
+      res.status(404).end('Listing was not found.');
+      return;
+    }
+    res.status(200).send({ listing }).end();
   });
 });
 
